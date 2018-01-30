29cfe56aabfd4ebe646ecd3874b8855399e36ba5
Zachary Hill <zach@zacharyhill.xyz>
2018-01-30 15:03:45 -0600

    Move verifyToken middleware into its own file

---
+++ controllers/middleware/verifyToken.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  /*
+  ** this allows us to send in our token via a header called "x-access-token"
+  ** or in the url in the form of "?token=${token}", now we can access the token
+  ** anywhere in our app by calling "process.env.TOKEN"
+  */
+    const token = req.headers['x-access-token'] || req.query.token;
+    process.env.TOKEN = token;
+  
+    /*
+    ** checks if token exists and if it has been signed as expected
+    ** i.e. it verifies token, or sends status 403 with error message in JSON
+    */
+    if (token) {
+      jwt.verify(token, process.env.SECRET_KEY, (err, decode) => {
+        if (!err) {
+          // decode gives you the value of the object the JWT token was signed with
+          next();
+        }
+        else {
+          res.status(403).json({ error: "invalid token" });
+        }
+      });
+    }
+    else {
+      res.status(403).json({ error: 'please send a token' });
+    }
+  };
--- routes/secureApi.js
+++ routes/secureApi.js
@@ -6,6 +6,8 @@ const s3etm = require('s3-emails-to-mongo');
 const getMailFromDB = require('../controllers/getMailFromDB');
 const checkToken = require('../controllers/checkToken');
 
+const verifyTokenMiddleware = require('../controllers/middleware/verifyToken');
+
 const secureRoutes = Router();
 
 // MAKE THIS PART DYNAMIC LATER
@@ -19,34 +21,7 @@ s3etm.configure({
 ** this middleware will be run BEFORE our route logic is hit below
 ** it will of course only run on routes defined with "serverRoutes" variable
 */
-secureRoutes.use((req, res, next) => {
-/*
-** this allows us to send in our token via a header called "x-access-token"
-** or in the url in the form of "?token=${token}", now we can access the token
-** anywhere in our app by calling "process.env.TOKEN"
-*/
-  const token = req.headers['x-access-token'] || req.query.token;
-  process.env.TOKEN = token;
-
-  /*
-  ** checks if token exists and if it has been signed as expected
-  ** i.e. it verifies token, or sends status 403 with error message in JSON
-  */
-  if (token) {
-    jwt.verify(token, process.env.SECRET_KEY, (err, decode) => {
-      if (!err) {
-        // decode gives you the value of the object the JWT token was signed with
-        next();
-      }
-      else {
-        res.status(403).json({ error: "invalid token" });
-      }
-    });
-  }
-  else {
-    res.status(403).json({ error: 'please send a token' });
-  }
-});
+secureRoutes.use(verifyTokenMiddleware);
 
 
 
