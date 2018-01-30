bce30ae6185868f7e96a225192dbf4ee723540e8
Zachary Hill <zach@zacharyhill.xyz>
2018-01-30 15:12:26 -0600

    Fix s3etm configuration to be in getNewMail function

--- controllers/getNewMail.js
+++ controllers/getNewMail.js
@@ -1,5 +1,10 @@
 const s3etm = require('s3-emails-to-mongo');
 
+// MAKE THIS PART DYNAMIC LATER
+s3etm.configure({
+  Bucket: 'zhillb-mail',
+});
+
 module.exports = async (req, res) => {
   try {
     const newMail = await s3etm();
