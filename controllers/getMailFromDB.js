e8725378b4a840ef2d5a181df3862af9294760f1
Zachary Hill <zach@zacharyhill.xyz>
2018-02-01 17:09:37 -0600

    Return json in proper format

--- controllers/getMailFromDB.js
+++ controllers/getMailFromDB.js
@@ -3,7 +3,7 @@ const Mail = require('../models/mail');
 module.exports = async (req, res, next) => {
   try {
     const allMail = await Mail.find({});
-    res.json(allMail);
+    res.json({ emails: allMail });
   } catch(err) {
     next(err); // pass onto next middleware function
   }
