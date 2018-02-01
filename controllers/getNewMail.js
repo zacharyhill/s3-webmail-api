de4e01d2ec3eb38153673cbc8ba001b4bf361e2f
Zachary Hill <zach@zacharyhill.xyz>
2018-02-01 17:09:50 -0600

    Remove saving attachments functionality
    Move save attachments functionality to s3-emails-to-mongo
    module. It is causing issues to save the buffer in the db.
    Now attachments will be saved in the db as the CID (which
    is being used as the filename in the specified directory)

--- controllers/getNewMail.js
+++ controllers/getNewMail.js
@@ -1,8 +1,4 @@
-const fs = require('fs');
-const path = require('path');
-const { promisify } = require('util');
 const s3etm = require('s3-emails-to-mongo');
-const writeFileAsync = promisify(fs.writeFile);
 
 // MAKE THIS PART DYNAMIC LATER
 s3etm.configure({
@@ -12,21 +8,9 @@ s3etm.configure({
 module.exports = async (req, res, next) => {
   try {
     const newMail = await s3etm();
-    // check if new mail has attachment(s), if so save them
-    await Promise.all(newMail.map((msg) => {
-      if (msg.attachments.length) {
-        return Promise.all(msg.attachments.map((attachment) => {
-          const file = attachment.content;
-          const cid = attachment.cid;
-          const filePath = path.join(__dirname, '..', 'data/uploads', cid);
-          return writeFileAsync(filePath, file).catch((err) => {
-              throw err;
-          });
-        }));
-      }
-    }));
     res.json({
       new: newMail.length,
+      emails: newMail,
     });
   } catch(err) {
       next(err);
