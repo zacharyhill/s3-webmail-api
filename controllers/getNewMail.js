d62d42bf1c2542588d6b39a7d44a11cf2341b627
Zachary Hill <zach@zacharyhill.xyz>
2018-01-31 19:48:37 -0600

    Add functionality to save attachments as mail is indexed
    Currently the new mail route only returns the number of messages
    that were indexed in mongodb. For some reason, `res.json` hangs
    anytime there is any attachments on the email, but works fine
    when there isn't. I think it may have something to do with the file
    buffer not being changed to JSON

--- controllers/getNewMail.js
+++ controllers/getNewMail.js
@@ -1,15 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
 const s3etm = require('s3-emails-to-mongo');
+const writeFileAsync = promisify(fs.writeFile);
 
 // MAKE THIS PART DYNAMIC LATER
 s3etm.configure({
-  Bucket: 'zhillb-mail',
+    Bucket: 'zhillb-mail',
 });
 
-module.exports = async (req, res) => {
+module.exports = async (req, res, next) => {
   try {
     const newMail = await s3etm();
-    res.json(newMail);
+    // check if new mail has attachment(s), if so save them
+    await Promise.all(newMail.map((msg) => {
+      if (msg.attachments.length) {
+        return Promise.all(msg.attachments.map((attachment) => {
+          const file = attachment.content;
+          const cid = attachment.cid;
+          const filePath = path.join(__dirname, '..', 'data/uploads', cid);
+          return writeFileAsync(filePath, file).catch((err) => {
+              throw err;
+          });
+        }));
+      }
+    }));
+    res.json({
+      new: newMail.length,
+    });
   } catch(err) {
-    net(err);
+      next(err);
   }
 };
